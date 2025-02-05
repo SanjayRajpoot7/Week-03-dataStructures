dad98ae777d8e21ec775f2cca12d8571b2e294ec
Sanjay rajpoot <sanjayrajpoot1165@gmail.com>
2025-02-05 12:27:12 +0530

    Some changes done

--- Day03-SortingAlgorithm/src/InsertionSort/SortEmployeeId/Main.java
+++
@@ -1,9 +0,0 @@
-package InsertionSort.SortEmployeeId;
-
-public class Main {
-    public static void main(String[] args) {
-        int[] employeeId = {73,21,3,1,2,53};
-        SortEmployeeId sortEmployeeId = new SortEmployeeId();
-        sortEmployeeId.sortEmployeeId(employeeId);
-    }
-}
